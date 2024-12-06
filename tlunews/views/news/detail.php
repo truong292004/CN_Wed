bf446eb4fdc956c84e2d9b673e925a29f29d5a00
ThangNM <nmt.minhthang01@gmail.com>
2024-12-06 22:51:26 +0700

    ThangNM: upcode chuc nang new

--- tlunews/views/news/detail.php
+++ tlunews/views/news/detail.php
@@ -1,1 +1,8 @@
+<h1><?= htmlspecialchars($news['title']) ?></h1>
+<img src="uploads/<?= htmlspecialchars($news['image']) ?>" alt="<?= htmlspecialchars($news['title']) ?>" style="width: 300px; height: auto;">
+<p><?= htmlspecialchars($news['content']) ?></p>
+<p><strong>Danh mục:</strong> <?= htmlspecialchars($news['category_name']) ?></p>
+<p><strong>Ngày đăng:</strong> <?= htmlspecialchars($news['created_at']) ?></p>
+<a href="index.php?controller=news&action=index">Quay lại danh sách</a>
+
 
