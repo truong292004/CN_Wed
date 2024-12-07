9047bfe118d7d0b22f772ea330b64108ce854369
truong292004 <namca9n@gmail.com>
2024-12-07 19:55:21 +0700

    Update User.php

--- tlunews/models/User.php
+++ tlunews/models/User.php
@@ -1,1 +1,22 @@
+<?php
+class User {
+    private $pdo;
 
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function login($username, $password) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE username = :username LIMIT 1");
+        $stmt->bindParam(":username", $username);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            if ($password === $row['password']) { // Sử dụng password_hash() và password_verify() trong sản xuất
+                return $row;
+            }
+        }
+        return false;
+    }
+}
+?>
