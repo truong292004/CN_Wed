782e81714111bd54892d53ae16359e4bc365f818
Iron045 <119028352+Iron045@users.noreply.github.com>
2024-12-07 20:59:08 +0700

    Update index.php

--- tlunews/index.php
+++ tlunews/index.php
@@ -1,31 +1,3 @@
-<?php
-$controller = $_GET['controller'] ?? 'news';
-$action = $_GET['action'] ?? 'index';
-
-switch ($controller) {
-    case 'news':
-        require_once 'controllers/NewsController.php';
-        $newsController = new NewsController();
-        switch ($action) {
-            case 'index':
-                $newsController->index();
-                break;
-            case 'detail':
-                $id = $_GET['id'] ?? 0;
-                $newsController->detail($id);
-                break;
-            case 'search':
-                $newsController->search();
-                break;
-            default:
-                echo "Hành động không hợp lệ.";
-                
-        }
-        break;
-    default:
-        echo "Controller không hợp lệ.";
-}
-
 
 <?php
 session_start();
