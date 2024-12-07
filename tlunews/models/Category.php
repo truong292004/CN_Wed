815cc66469a601a3633acaf6d3f3c9e7e2d0045d
Minhthang0123 <145792795+Minhthang0123@users.noreply.github.com>
2024-12-07 20:52:55 +0700

    Update Category.php

--- tlunews/models/Category.php
+++ tlunews/models/Category.php
@@ -1,1 +1,35 @@
 
+<?php
+class Category {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getAll() {
+        return $this->pdo->query("SELECT * FROM categories")->fetchAll();
+    }
+
+    public function getById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM categories WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public function add($name) {
+        $stmt = $this->pdo->prepare("INSERT INTO categories (name) VALUES (?)");
+        return $stmt->execute([$name]);
+    }
+
+    public function update($id, $name) {
+        $stmt = $this->pdo->prepare("UPDATE categories SET name = ? WHERE id = ?");
+        return $stmt->execute([$name, $id]);
+    }
+
+    public function delete($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM categories WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
+}
+?>
